950998b5d0973645cb6c2eb421f3277a112f39b0
Surfer0997 <artanagibaet228@gmail.com>
2023-05-09 18:13:06 +0300

    imp: messages scroll into view

--- src/components/ChatWindow/ChatBubblesContainer.tsx
+++ src/components/ChatWindow/ChatBubblesContainer.tsx
@@ -3,17 +3,25 @@ import ChatBubble from './ChatBubble';
 import MyChatBubble from './MyChatBubble';
 import { RootState } from '@/store/store';
 import { v4 as uuidv4 } from 'uuid';
+import {useRef, useEffect} from 'react';
 
 const ChatBubblesContainer = () => {
   const user = useSelector((state: RootState) => state.user);
   const currentConversation = useSelector((state:RootState)=>state.currentConversation.conversation);
+  const containerRef = useRef<HTMLDivElement | null>(null)
+  
+  useEffect(()=>{
+    containerRef.current?.lastElementChild?.scrollIntoView({behavior:'smooth'});
+  }, [currentConversation.messages])
+
   if (!currentConversation.messages) return <p>Please, open conversation to start</p>
   return (
     <div
-      className="py-2"
+      className="pt-2"
       style={{
         width: 'calc(100% - 52px)',
       }}
+      ref={containerRef}
     >
       {currentConversation.messages.length === 0 && 'Send message to start conversation!'}
       {currentConversation.messages.map(message => {
