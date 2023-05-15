2b16215bfee72133eae1d8fcad463c9c90b029a3
Surfer0997 <artanagibaet228@gmail.com>
2023-05-15 22:56:55 +0300

    imp: sending date of message

--- src/components/ChatWindow/ChatBubblesContainer.tsx
+++ src/components/ChatWindow/ChatBubblesContainer.tsx
@@ -4,7 +4,6 @@ import MyChatBubble from './MyChatBubble';
 import { RootState } from '@/store/store';
 import { v4 as uuidv4 } from 'uuid';
 import { useRef, useEffect } from 'react';
-import { NoConversation } from './NoConversation';
 
 const ChatBubblesContainer = () => {
   const user = useSelector((state: RootState) => state.user);
@@ -14,7 +13,7 @@ const ChatBubblesContainer = () => {
   useEffect(() => {
     containerRef.current?.lastElementChild?.scrollIntoView({ behavior: 'smooth' });
   }, [currentConversation.messages]);
-
+console.log(navigator.language);
   return (
     <div
       className="pt-12 max-sm:mx-auto"
@@ -26,9 +25,15 @@ const ChatBubblesContainer = () => {
       {currentConversation.messages.length === 0 && 'Send message to start conversation!'}
       {currentConversation.messages.map(message => {
         return message.sender === user.data._id ? (
-          <MyChatBubble key={message._id ? message._id : uuidv4()} text={message.text} />
+          <MyChatBubble key={message._id ? message._id : uuidv4()} text={message.text} date={new Date(message.date).toLocaleTimeString(navigator.language, {
+            hour: '2-digit',
+            minute: '2-digit',
+          })}/>
         ) : (
-          <ChatBubble key={message._id ? message._id : uuidv4()} text={message.text} />
+          <ChatBubble key={message._id ? message._id : uuidv4()} text={message.text} date={new Date(message.date).toLocaleTimeString(navigator.language, {
+            hour: '2-digit',
+            minute: '2-digit',
+          })}/>
         );
       })}
     </div>
--- src/components/ChatWindow/MyChatBubble.tsx
+++ src/components/ChatWindow/MyChatBubble.tsx
@@ -1,9 +1,15 @@
 export interface BubbleProps {
   text: string;
+  date: string;
 }
 
 const MyChatBubble = (props: BubbleProps) => {
-  return <div className="bg-white rounded-xl w-2/3 ml-auto mr-2 mt-2 pl-3 max-sm:pl-2 p-2 dark:bg-gradient-to-r dark:from-indigo-600 dark:via-purple-600 dark:to-pink-500 whitespace-pre">{props.text}</div>;
+  return (
+    <div className=" relative bg-white rounded-xl w-2/3 ml-auto mr-2 mt-2 pl-3 max-sm:pl-2 p-2 dark:bg-gradient-to-r dark:from-indigo-600 dark:via-purple-600 dark:to-pink-500 whitespace-pre">
+      {props.text}
+      <p className="absolute bottom-1 right-2 text-xs dark:text-white">{props.date}</p>
+    </div>
+  );
 };
 
 export default MyChatBubble;
