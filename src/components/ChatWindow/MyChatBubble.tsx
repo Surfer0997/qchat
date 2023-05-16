488ba285f2d78c10acddc90932c8bde4fbdd8a4e
Surfer0997 <artanagibaet228@gmail.com>
2023-05-16 15:24:40 +0300

    fix: msg bubbles lines

--- src/components/ChatWindow/MyChatBubble.tsx
+++ src/components/ChatWindow/MyChatBubble.tsx
@@ -5,7 +5,7 @@ export interface BubbleProps {
 
 const MyChatBubble = (props: BubbleProps) => {
   return (
-    <div className=" relative bg-white rounded-xl w-2/3 ml-auto mr-2 mt-2 pl-3 max-sm:pl-2 p-2 dark:bg-gradient-to-r dark:from-indigo-600 dark:via-purple-600 dark:to-pink-500 whitespace-pre">
+    <div className=" relative bg-white rounded-xl w-2/3 ml-auto mr-2 mt-2 pl-3 max-sm:pl-2 p-2 dark:bg-gradient-to-r dark:from-indigo-600 dark:via-purple-600 dark:to-pink-500 whitespace-pre-line">
       {props.text}
       <p className="absolute bottom-1 right-2 text-xs dark:text-white">{props.date}</p>
     </div>
